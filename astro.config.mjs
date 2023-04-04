1235e3f0dc8c4c052395b4b5926e5685a2736d69
hfellerhoff <henryfellerhoff@gmail.com>
2023-04-04 01:29:08 -0400

    add pwa, favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import solidJs from '@astrojs/solid-js';
-import preact from '@astrojs/preact';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import preact from "@astrojs/preact";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), preact(), svelte()],
+  integrations: [AstroPWA(), tailwind(), solidJs(), preact(), svelte()],
   vite: {
     ssr: {
-      external: ['svgo'],
+      external: ["svgo"],
     },
   },
 });

c548846f2e6a2c26d23035a8b5341dabd2dde182
hfellerhoff <henryfellerhoff@gmail.com>
2023-06-28 01:12:59 -0400

    update theramin

--- src/features/theramin/index.tsx
+++ src/features/theramin/index.tsx
@@ -1,242 +1,370 @@
-import * as Tone from 'tone'
-import { createSignal, lazy, onMount } from 'solid-js';
+import * as Tone from "tone";
+import { For, createMemo, createSignal, onMount } from "solid-js";
+// @ts-ignore
+import Handsfree from "handsfree";
+import clsx from "clsx";
+import {
+  FaRegularHand,
+  FaRegularHandLizard,
+  FaSolidArrowsLeftRight,
+  FaSolidArrowsUpDown,
+  FaSolidMapPin,
+  FaSolidWaveSquare,
+} from "solid-icons/fa";
+import FloatingModuleWrapper from "@modules/FloatingModuleWrapper";
+import Select from "@components/Select";
+
+enum FINGERS {
+  WRIST,
+  THUMB_CMC,
+  THUMB_MCP,
+  THUMB_IP,
+  THUMB_TIP,
+  INDEX_FINGER_MCP,
+  INDEX_FINGER_PIP,
+  INDEX_FINGER_DIP,
+  INDEX_FINGER_TIP,
+  MIDDLE_FINGER_MCP,
+  MIDDLE_FINGER_PIP,
+  MIDDLE_FINGER_DIP,
+  MIDDLE_FINGER_TIP,
+  RING_FINGER_MCP,
+  RING_FINGER_PIP,
+  RING_FINGER_DIP,
+  RING_FINGER_TIP,
+  PINKY_MCP,
+  PINKY_PIP,
+  PINKY_DIP,
+  PINKY_TIP,
+  CENTER_PALM,
+}
 
 const defaultParams = {
-    flipHorizontal: false,
-    outputStride: 16,
-    imageScaleFactor: 1,
-    maxNumBoxes: 20,
-    iouThreshold: 0.2,
-    scoreThreshold: 0.6,
-    modelType: "ssd320fpnlite",
-    modelSize: "large",
-    bboxLineWidth: "2",
-    fontSize: 17,
+  flipHorizontal: false,
+  outputStride: 16,
+  imageScaleFactor: 1,
+  maxNumBoxes: 20,
+  iouThreshold: 0.2,
+  scoreThreshold: 0.4,
+  modelType: "ssd320fpnlite",
+  modelSize: "large",
+  bboxLineWidth: "2",
+  fontSize: 17,
 };
 
 type ActiveNote = {
-    pitch: number,
-    volume: number,
-    gain: Tone.Gain
-    oscillator: Tone.Oscillator
-    signal: Tone.Signal<"frequency">
-}
+  pitch: number;
+  volume: number;
+  gain: Tone.Gain;
+  oscillator: Tone.PWMOscillator;
+  signal: Tone.Signal<"frequency">;
+};
 
-const MAX_PITCH = 1260
-const MIN_PITCH = 40
+const MAX_PITCH = 1260;
+const MIN_PITCH = 40;
 
 type Prediction = {
-    label: string,
-    x: number,
-    y: number,
-    width: number,
-    height: number
-}
+  label: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
-const frequencies = new Array(120).fill(0).map((_, i) => 40 + i).map(noteNumber => {
-    return 440 * 2 ** ((noteNumber - 69) / 12)
-})
+const frequencies = new Array(120)
+  .fill(0)
+  .map((_, i) => 40 + i)
+  .map((noteNumber) => {
+    return 440 * 2 ** ((noteNumber - 69) / 12);
+  });
 
 const autotune = (frequency: number) => {
-    return frequencies.reduce((prev, curr) => {
-        return (Math.abs(curr - frequency) < Math.abs(prev - frequency) ? curr : prev);
-    });
+  return frequencies.reduce((prev, curr) => {
+    return Math.abs(curr - frequency) < Math.abs(prev - frequency)
+      ? curr
+      : prev;
+  });
+};
+
+function clamp(num: number, min: number, max: number) {
+  return num <= min ? min : num >= max ? max : num;
 }
 
-function getPitchFromPrediction(prediction: Prediction) {
-    let pitchFrequency = (prediction.x + (prediction.width / 2)) / window.innerWidth
-    let pitchVolume = (600 - (prediction.y + prediction.width / 2)) / 600
+const OSCILLATORS = [
+  {
+    createOscillator: () => new Tone.Oscillator(),
+    label: "Oscillator",
+  },
+  {
+    createOscillator: () => new Tone.AMOscillator(),
+    label: "AM Oscillator",
+  },
+  {
+    createOscillator: () => new Tone.FMOscillator(),
+    label: "FM Oscillator",
+  },
+  {
+    createOscillator: () => new Tone.FatOscillator(),
+    label: "Fat Oscillator",
+  },
+  {
+    createOscillator: () => new Tone.PWMOscillator(60, 1),
+    label: "Pulse Oscillator",
+  },
+];
 
-    if (pitchFrequency < 0) pitchFrequency = 0
-    if (pitchFrequency > 1066) pitchFrequency = 1066
+type Landmark = {
+  x: number;
+  y: number;
+  z: number;
+};
 
-    const interpolatedFrequency = pitchFrequency * MAX_PITCH
-    return autotune(interpolatedFrequency)
-}
+const createAudioElements = (template: string) => {
+  const oscillator = OSCILLATORS.find(
+    (osc) => osc.label === template
+  )!.createOscillator();
 
-function getVolumeFromPrediction(prediction: Prediction) {
-    let pitchVolume = (600 - (prediction.y + prediction.width / 2)) / 600
+  const gain = new Tone.Gain(0).toDestination();
+  const tremolo = new Tone.Tremolo(9, 0).connect(gain).start();
+  const crusher = new Tone.BitCrusher(16).connect(tremolo);
+  const osc = oscillator.connect(crusher).start();
+  const signal = new Tone.Signal({
+    units: "frequency",
+  }).connect(osc.frequency);
 
-    if (pitchVolume < 0) pitchVolume = 0
-    if (pitchVolume > 1) pitchVolume = 1
-
-    return pitchVolume
-}
-
-const createActiveNote = (pitch: number, volume: number): ActiveNote => {
-    const gain = new Tone.Gain(0).toDestination()
-    const osc = new Tone.Oscillator().connect(gain).start();
-    // a scheduleable signal which can be connected to control an AudioParam or another Signal
-    const signal = new Tone.Signal({
-        units: "frequency"
-    }).connect(osc.frequency);
-
-    return {
-        pitch,
-        volume,
-        gain,
-        oscillator: osc,
-        signal
-    }
-}
+  return {
+    gain,
+    tremolo,
+    crusher,
+    osc,
+    signal,
+  };
+};
 
 export default function TheraminApp() {
-    const [activeNotes, setActiveNotes] = createSignal<ActiveNote[]>([])
-    const [videoWidth, setVideoWidth] = createSignal(window.innerWidth)
-    const [videoHeight, setVideoHeight] = createSignal(window.innerHeight)
+  const hands = new Array(2).fill(0).map((_, i) => i);
+  const fingers = new Array(22).fill(0).map((_, i) => i);
 
-    const start = async () => {
-        const videoElement = document.querySelector('video');
+  const [audioElements, setAudioElements] = createSignal(
+    createAudioElements("Oscillator")
+  );
 
-        // @ts-ignore
-        if (!window.handTrack?.load) {
-            setTimeout(start, 100);
-            return;
-        }
+  const onChangeOscillator = (event: Event) => {
+    const template = (event.target as HTMLSelectElement).value;
 
-        // @ts-ignore
-        const model = await window.handTrack?.load(defaultParams)
-        // @ts-ignore
-        window.handTrack.startVideo(videoElement)
+    const { gain, tremolo, crusher, osc, signal } = audioElements();
+    signal.dispose();
+    osc.dispose();
+    crusher.dispose();
+    tremolo.dispose();
+    gain.dispose();
 
-        videoElement?.setAttribute("style", "height: 100vh;")
-        // open
-        // closed
-        // pinch
-        // point
-        // face
-        // pointtip
-        // pinchtip
+    setAudioElements(createAudioElements(template));
+  };
 
-        Tone.Transport.scheduleRepeat(async () => {
-            const rawPredictions = await model.detect(videoElement) as {
-                label: string,
-                bbox: [number, number, number, number]
-            }[];
+  onMount(async () => {
+    const handsfree = new Handsfree({ hands: true });
+    handsfree.start();
 
-            const updatedPredictions: {
-                label: string,
-                x: number,
-                y: number,
-                width: number,
-                height: number
-            }[] = rawPredictions.map(p => ({
-                label: p.label,
-                x: p.bbox[0],
-                y: p.bbox[1],
-                width: p.bbox[2],
-                height: p.bbox[3]
-            })).filter(p => p.label === 'closed')
+    const box = document.getElementById("box");
+    const boxes: Record<string, HTMLElement> = {};
 
-            const closestPredictionToActiveNote = activeNotes().map(activeNote => {
-                return {
-                    activeNote,
-                    closestPrediction: updatedPredictions.reduce((prev, curr) => {
-                        const prevDistance = Math.sqrt((activeNote.pitch - prev.x) ** 2 + (activeNote.volume - prev.y) ** 2)
-                        const currDistance = Math.sqrt((activeNote.pitch - curr.x) ** 2 + (activeNote.volume - curr.y) ** 2)
-    
-                        return prevDistance < currDistance ? prev : curr
-                    })
-                }
-            })
-
-            if (updatedPredictions.length < activeNotes().length) {
-                const closestActiveNotesToPredictions = updatedPredictions.map(updatedPrediction => {
-                    return {
-                        updatedPrediction,
-                        closestActiveNote: activeNotes().reduce((prev, curr) => {
-                            const prevDistance = Math.sqrt((updatedPrediction.x - prev.pitch) ** 2 + (updatedPrediction.y - prev.volume) ** 2)
-                            const currDistance = Math.sqrt((updatedPrediction.x - curr.pitch) ** 2 + (updatedPrediction.y - curr.volume) ** 2)
-
-                            return prevDistance < currDistance ? prev : curr
-                        })
-                    }
-                })
-
-                // for each active note not associated with a prediction, remove the active note
-                const activeNotesWithoutPredictions = activeNotes().filter(activeNote => {
-                    return !closestActiveNotesToPredictions.some(closestActiveNoteToPrediction => {
-                        return closestActiveNoteToPrediction.closestActiveNote === activeNote
-                    })
-                })
-
-                activeNotesWithoutPredictions.forEach(activeNote => {
-                    activeNote.oscillator.stop()
-                    activeNote.gain.gain.rampTo(0, 0.1)
-
-                    activeNote.signal.dispose()
-                    activeNote.gain.dispose()
-                    activeNote.oscillator.dispose()
-                })
-
-                setActiveNotes(activeNotes().filter(activeNote => {
-                    return !activeNotesWithoutPredictions.some(activeNoteWithoutPrediction => {
-                        return activeNoteWithoutPrediction === activeNote
-                    })
-                }))
-            } else if (updatedPredictions.length > activeNotes().length) {
-                // find closest prediction for each active note
-                const closestPredictionsToActiveNotes = activeNotes().map(activeNote => {
-                    return {
-                        activeNote,
-                        closestPrediction: updatedPredictions.reduce((prev, curr) => {
-                            const prevDistance = Math.sqrt((activeNote.pitch - prev.x) ** 2 + (activeNote.volume - prev.y) ** 2)
-                            const currDistance = Math.sqrt((activeNote.pitch - curr.x) ** 2 + (activeNote.volume - curr.y) ** 2)
-        
-                            return prevDistance < currDistance ? prev : curr
-                        })
-                    }
-                })
-
-                // for each prediction not associated with an active note, create a new active note
-                const predictionsWithoutActiveNotes = updatedPredictions.filter(updatedPrediction => {
-                    return !closestPredictionsToActiveNotes.some(closestPredictionToActiveNote => {
-                        return closestPredictionToActiveNote.closestPrediction === updatedPrediction
-                    })
-                })
-
-                predictionsWithoutActiveNotes.forEach(predictionWithoutActiveNote => {
-                    const frequency = getPitchFromPrediction(predictionWithoutActiveNote)
-                    const volume = getVolumeFromPrediction(predictionWithoutActiveNote)
-
-                    setActiveNotes([...activeNotes(), createActiveNote(frequency, volume)])
-                })
-            }
-
-            activeNotes().forEach(activeNote => {
-                const closestPrediction = closestPredictionToActiveNote.find(closestPredictionToActiveNote => {
-                    return closestPredictionToActiveNote.activeNote === activeNote
-                })?.closestPrediction
-
-                if (closestPrediction) {
-                    const frequency = getPitchFromPrediction(closestPrediction)
-                    const volume = getVolumeFromPrediction(closestPrediction)
-
-                    activeNote.pitch = frequency
-                    activeNote.volume = volume
-
-                    activeNote.signal.rampTo(frequency, 0.1, Tone.now())
-                    activeNote.gain.gain.rampTo(volume, 0.1, Tone.now())
-                }
-            })
-
-            // console.log(activeNotes().map(activeNote => `${activeNote.pitch.toFixed(2)}Hz, ${activeNote.volume.toFixed(2)}`).join('\n'))
-
-            setActiveNotes(activeNotes() || [])
-        }, 0.05)    
-        Tone.Transport.start() 
+    for (const hand of hands) {
+      for (const finger of fingers) {
+        boxes[`${hand}-${finger}`] = document.getElementById(
+          `${hand}-${finger}`
+        )!;
+      }
     }
 
-    onMount(async () => {
-           start()
+    const handleRightHandFinger = (
+      index: number,
+      fingerState: Landmark,
+      handState: {
+        startedPinch: boolean;
+        holdingPinch: boolean;
+        releasedPinch: boolean;
+      },
+      { gain, tremolo, signal }: ReturnType<typeof audioElements>
+    ) => {
+      if (index === FINGERS.CENTER_PALM) {
+        const frequencyValue = 100 + fingerState.x * 500;
+        const gainAdjustment = 100 / frequencyValue;
+        const gainValue = 1 - fingerState.y + gainAdjustment;
 
-           window.addEventListener('resize', () => {
-                setVideoWidth(window.innerWidth)
-                setVideoHeight(window.innerHeight)
-           })
-    })
+        if (handState.startedPinch) {
+          signal.rampTo(frequencyValue, 0.1, Tone.now());
+          gain.gain.rampTo(gainValue, 0.1, Tone.now());
+          tremolo.depth.rampTo(0, 0.1, Tone.now());
+        }
+        if (handState.releasedPinch) {
+          gain.gain.rampTo(0, 0.1, Tone.now());
+        }
+        if (handState.holdingPinch) {
+          gain.gain.rampTo(gainValue, 0.1, Tone.now());
+          signal.rampTo(frequencyValue, 0.1, Tone.now());
+        }
+      }
+    };
 
-    return (
-        <video width={videoWidth()} height={videoHeight()} class="bg-gray-900 object-cover scale-x-[-1]"  />
-    )
+    const handleLeftHandFinger = (
+      index: number,
+      fingerState: Landmark,
+      handState: {
+        startedPinch: boolean;
+        holdingPinch: boolean;
+        releasedPinch: boolean;
+      },
+      { tremolo, crusher }: ReturnType<typeof audioElements>
+    ) => {
+      const tremoloValue = 1 - fingerState.y;
+      const crusherValue = clamp(Math.round(fingerState.x ** 2 * 16), 1, 16);
+
+      if (index === FINGERS.CENTER_PALM) {
+        if (handState.startedPinch) {
+          tremolo.depth.rampTo(tremoloValue, 0.1, Tone.now());
+          crusher.bits.rampTo(crusherValue, 0.1, Tone.now());
+        }
+        if (handState.releasedPinch) {
+          tremolo.depth.rampTo(0, 0.1, Tone.now());
+          crusher.bits.rampTo(16, 0.1, Tone.now());
+        }
+        if (handState.holdingPinch) {
+          tremolo.depth.rampTo(tremoloValue, 0.1, Tone.now());
+          crusher.bits.rampTo(crusherValue, 0.1, Tone.now());
+        }
+      }
+    };
+
+    handsfree.use(
+      "logger",
+      (data: {
+        hands: {
+          landmarksVisible: boolean[];
+          landmarks: Landmark[][];
+          pinchState: string[][];
+        };
+      }) => {
+        if (!data.hands) {
+          return;
+        }
+
+        const toneAudioElements = audioElements();
+
+        data.hands.landmarksVisible?.forEach((visible, i) => {
+          if (visible) {
+            data.hands.landmarks[i]?.forEach((landmark, j) => {
+              boxes[`${i}-${j}`]?.setAttribute(
+                "style",
+                `top: ${landmark.y * 100}%; left: ${(1 - landmark.x) * 100}%`
+              );
+              if (i === 0) {
+                handleLeftHandFinger(
+                  j,
+                  landmark,
+                  {
+                    startedPinch: data.hands.pinchState?.[i]?.[0] === "start",
+                    holdingPinch: data.hands.pinchState?.[i]?.[0] === "held",
+                    releasedPinch:
+                      data.hands.pinchState?.[i]?.[0] === "released",
+                  },
+                  toneAudioElements
+                );
+              }
+              if (i === 1) {
+                handleRightHandFinger(
+                  j,
+                  landmark,
+                  {
+                    startedPinch: data.hands.pinchState?.[i]?.[0] === "start",
+                    holdingPinch: data.hands.pinchState?.[i]?.[0] === "held",
+                    releasedPinch:
+                      data.hands.pinchState?.[i]?.[0] === "released",
+                  },
+                  toneAudioElements
+                );
+              }
+            });
+          } else {
+            if (i === 1) {
+              toneAudioElements.gain.gain.rampTo(0, 0.1, Tone.now());
+            }
+          }
+        });
+      }
+    );
+  });
+
+  return (
+    <>
+      <For each={hands}>
+        {(hand) => (
+          <For each={fingers}>
+            {(finger) => (
+              <div
+                id={`${hand}-${finger}`}
+                class={clsx(
+                  "absolute rounded-full h-4 w-4 text-white grid place-items-center",
+                  {
+                    "bg-red-500 dark:bg-red-400": hand === 0,
+                    "bg-blue-500 dark:bg-blue-400": hand === 1,
+                  }
+                )}
+              />
+            )}
+          </For>
+        )}
+      </For>
+      <FloatingModuleWrapper icon={<FaSolidWaveSquare />} position="top-right">
+        <label
+          for="scale-select"
+          class="block text-sm font-medium text-gray-500 dark:text-gray-400"
+        >
+          Scale
+        </label>
+        <Select id="scale-select" onChange={onChangeOscillator}>
+          <For each={OSCILLATORS}>
+            {(template) => (
+              <option value={template.label}>{template.label}</option>
+            )}
+          </For>
+        </Select>
+      </FloatingModuleWrapper>
+      <FloatingModuleWrapper
+        icon={<FaRegularHand class="scale-x-[-1]" />}
+        position="bottom-left"
+      >
+        <div class="text-8 font-bold uppercase text-gray-400 dark:text-gray-500">
+          Left Hand
+        </div>
+        <div class="flex flex-col gap-1 mb-2">
+          <div class="flex items-center gap-2">
+            <FaRegularHandLizard /> Start/Stop Effects
+          </div>
+          <div class="flex items-center gap-2">
+            <FaSolidArrowsUpDown /> Tremolo
+          </div>
+          <div class="flex items-center gap-2">
+            <FaSolidArrowsLeftRight /> Bitcrusher
+          </div>
+        </div>
+      </FloatingModuleWrapper>
+      <FloatingModuleWrapper icon={<FaRegularHand />} position="bottom-right">
+        <div class="text-8 font-bold uppercase text-gray-400 dark:text-gray-500">
+          Right Hand
+        </div>
+        <div class="flex flex-col gap-1 mb-2">
+          <div class="flex items-center gap-2">
+            <FaRegularHandLizard /> Start/Stop Pitch
+          </div>
+          <div class="flex items-center gap-2">
+            <FaSolidArrowsUpDown /> Volume
+          </div>
+          <div class="flex items-center gap-2">
+            <FaSolidArrowsLeftRight /> Frequency
+          </div>
+        </div>
+      </FloatingModuleWrapper>
+    </>
+  );
 }
